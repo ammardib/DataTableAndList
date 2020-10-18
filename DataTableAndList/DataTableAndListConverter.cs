ea5356e91861ac517d5cf84dac5902ff0c661231
Ammar Dib <ammardib@outlook.com>
2020-10-19 00:58:14 +0300

    init

---
+++ DataTableAndList/DataTableAndListConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+
+namespace DataTableAndList
+{
+    public static class DataTableAndListConverter
+    {
+        /// <summary>
+        /// Converts List to DataTable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_list"></param>
+        /// <returns></returns>
+        public static DataTable ConvertToDataTable<T>(List<T> _list)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor property in properties)
+            {
+                table.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+            }
+
+            foreach (T _row in _list)
+            {
+                DataRow dataRow = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    dataRow[prop.Name] = prop.GetValue(_row) ?? DBNull.Value;
+                }
+
+                table.Rows.Add(dataRow);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// Converts DataTable to List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        public static List<T> ConvertToList<T>(DataTable dataTable)
+        {
+            List<T> list = new List<T>();
+            var obj = default(T);
+            var type = typeof(T);
+            var fields = type.GetProperties();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                obj = Activator.CreateInstance<T>();
+                foreach (var fieldInfo in fields)
+                {
+                    foreach (DataColumn dc in dataTable.Columns)
+                    {
+                        if (fieldInfo.Name == dc.ColumnName)
+                        {
+                            // Get the value from the datatable cell
+                            object value = dr[dc.ColumnName];
+                            if (value.GetType() == typeof(DBNull))
+                                value = null;
+
+                            // Set the value into the object
+                            fieldInfo.SetValue(obj, value);
+                            break;
+                        }
+                    }
+                }
+                if (obj != null)
+                    list.Add(obj);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ DataTableAndList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DataTableAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Convert DataTable To List
+            var dt = Helper.GetSampleDataTable();
+            var _list = DataTableAndListConverter.ConvertToList<Employee>(dt);
+
+            //2. Convert List To DataTable
+            var list = Helper.GetSampleList();
+            var _dt = DataTableAndListConverter.ConvertToDataTable<Employee>(list);
+        }
+
+        public static class Helper
+        {
+            public static DataTable GetSampleDataTable()
+            {
+                DataTable dataTable = new DataTable();
+                dataTable.Columns.Add("EmployeeId", typeof(int));
+                dataTable.Columns.Add("EmployeeName", typeof(string));
+                dataTable.Columns.Add("EmployeeSalary", typeof(decimal));
+
+                dataTable.Rows.Add(12002, "Ivan", 5400);
+                dataTable.Rows.Add(12016, "Sam", 3800);
+                dataTable.Rows.Add(12092, "Eva", 4600);
+                dataTable.Rows.Add(12058, "Ana", 7200);
+                dataTable.Rows.Add(12047, "Brian", 5300);
+
+                return dataTable;
+            }
+
+            public static List<Employee> GetSampleList()
+            {
+                List<Employee>  employees = new List<Employee>();
+                employees.Add(new Employee { EmployeeId = 12002, EmployeeName = "Ivan", EmployeeSalary = 5400 });
+                employees.Add(new Employee { EmployeeId = 12016, EmployeeName = "Sam", EmployeeSalary = 3800 });
+                employees.Add(new Employee { EmployeeId = 12092, EmployeeName = "Eva", EmployeeSalary = 4600 });
+                employees.Add(new Employee { EmployeeId = 12058, EmployeeName = "Ana", EmployeeSalary = 7200 });
+                employees.Add(new Employee { EmployeeId = 12047, EmployeeName = "Brian", EmployeeSalary = 5300 });
+                
+                return employees;
+            }
+        }
+
+        public class Employee
+        {
+            public int EmployeeId { get; set; }
+            public string EmployeeName { get; set; }
+            public decimal EmployeeSalary { get; set; }
+        }
+    }
+
+    
+}
